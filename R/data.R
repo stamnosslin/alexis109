b60c94f7aeb90a435eb5a051182de6aad919cef6
stamnosslin <mats.nilsson@psychology.su.se>
2018-08-09 16:53:09 +0200

    Processed data files added

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
 #' Data from experiment ALEXIS 109: Lateralization experiment
 #'
-#' This is data from an experiment with three participants, tested on lateralization 
-#' of a large binaural difference in lag clicks following a lead click after 
-#' an inter-click interval between 0.125 to 256 ms. The binaural difference was 
-#' an ITD of 0.35 ms or an ILD of 10 dB or both. 
+#' This is data from an experiment with three participants, tested on lateralization
+#' of a large binaural difference in lag clicks following a lead click after
+#' an inter-click interval between 0.125 to 256 ms. The binaural difference was
+#' an ITD of 0.35 ms or an ILD of 10 dB or both.
 #'
 #' @format A data frame with 49,140 rows and 13 columns.
 #' \describe{
@@ -28,7 +28,7 @@
 #' Data from experiment ALEXIS 109: Detection experiment
 #'
 #' This is data from an experiment with three participants, tested on detection
-#' of a lag click following a lead click after an inter-click interval between 
+#' of a lag click following a lead click after an inter-click interval between
 #' 0.125 to 256 ms. The binaural difference in the lag-click was an ITD of 0.35 ms
 #' an ILD of 10 dB or both or neither.
 #'
@@ -54,9 +54,9 @@
 
 #' Data from experiment ALEXIS 109: Extra experiment with level roving
 #'
-#' This is data from an experiment with five participants, tested on tested on 
-#' lateralization of a large binaural difference in lag clicks following a lead click 
-#' after an inter-click interval of 4 ms. The binaural difference was 
+#' This is data from an experiment with five participants, tested on tested on
+#' lateralization of a large binaural difference in lag clicks following a lead click
+#' after an inter-click interval of 4 ms. The binaural difference was
 #' an ITD of 0.35 ms or an ILD of 10 dB or both. The overall level of the first
 #' and second interval was randomly and independently roved +/- 10 dB in the roving
 #' condition
@@ -82,3 +82,26 @@
 #'                 The answer was correct if answer == correct}
 #'   }
 "data109rov"
+
+#' Processed data from experiment ALEXIS 109: Lateralization experiment
+#'
+#' This is processed data (estimated thresholds) from an experiment with three participants, tested on lateralization
+#' of a large binaural difference in lag clicks following a lead click after
+#' an inter-click interval between 0.125 to 256 ms. The binaural difference was
+#' an ITD of 0.35 ms or an ILD of 10 dB or both.
+#'
+#' @format A data frame with 49,140 rows and 13 columns.
+#' \describe{
+#'   \item{id}{Participant unique id number}
+#'   \item{ici}{Inter-click interval in ms, Note: -1 is for sessions with no lead click}
+#'   \item{bincond}{Binaural condition: 1 = ILD-only, 2 = ITD-only, 3 = ITD+ILD}
+#'   \item{thmode}{Threshold estimate (mode of posterior distribution), i.e., LLR [dB] at 75 % correct responses}
+#'   \item{X.0.95}{Lower 95 % credibility interval of posterior distribution of threshold}
+#'   \item{X0.95.}{Upper 95 % credibility interval of posterior distribution of threshold}
+#'   \item{mmode}{Mode of location parameter m}
+#'   \item{wmode}{Mode of scale parameter w (spread)}
+#'   \item{lapsemode}{Mode of lapse-reate parameter lambda}
+#'   \item{mineff}{MCMC diagnostic: lowest (worst) effective sample size over estimated parameters}
+#'   \item{maxrhat}{MCMC diagnostic: highest (worst) rhat (should be very close to 1.00)}
+#'   }
+"ths_lat"
